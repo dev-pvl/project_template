84f968f458ff593ad5bcdfe6bc8faa64e775ab57
paul <pavel@psm7.com>
2017-03-06 11:21:47 +0200

    init

---
+++ src/js/common.js
@@ -0,0 +1,12 @@
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+//= ../../bower_components/slick-carousel/slick/slick.js
+
+
+/*
+ * Custom
+ */
+//= partials/app.js
